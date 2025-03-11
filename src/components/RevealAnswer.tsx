0c92783ca0b09ef8560c8b4c5a34a02aa08e3102
saieda <saieda@udel.edu>
2025-03-11 01:11:53 -0400

    RevealAnswer is updated.

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,12 +2,18 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    //Answer should not be visibly initially, so should be initialized to false
+    // Answer should not be visible initially, so should be initialized to false
     const [visible, setVisible] = useState<boolean>(false);
 
-    return (<div>
-        <Button onClick={()=> setVisible(!visible) }>Reveal Answer</Button>
-    {visible && <div>42!</div>}
-    </div>
+    return (
+        <div>
+            <Button onClick={() => {
+                setVisible(!visible);
+            }}>
+                Reveal Answer
+            </Button>
+            {visible && <div>42!</div>}
+        </div>
     );
 }
+
